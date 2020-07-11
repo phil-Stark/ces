707f57f50bddcfe3effaed114fc782b9ca0b6ea4
phil-Stark <39791007+phil-Stark@users.noreply.github.com>
2020-07-11 17:15:47 +0800

    Update HelloWorld.java
    全部删除

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -15,17 +15,6 @@ public class HelloWorld {
 
         System.out.println("第三次实验");
 
-        System.out.println("");
-
-        System.out.println("这都是情况");
-
-        System.out.println("为啥idea总是提交不上啊");
-
-        System.out.println("测试revert");
-
-
-        System.out.println("坑，不弹框，还要手动提");
-        System.out.println("先这样吧");
-        System.out.println("测试");
+  System.out.println("全部删除");
     }
 }
